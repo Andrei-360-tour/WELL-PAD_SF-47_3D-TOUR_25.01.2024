88592d449c406891ce99ef626c9efc56a8c2cb04
Andrei-360-tour <156935416+Andrei-360-tour@users.noreply.github.com>
2024-01-30 16:18:56 +0300

    Commit

---
+++ data.js
@@ -0,0 +1,637 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sf-47_overview",
+      "name": "SF-47_Overview",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.2516783396308959,
+        "pitch": 0.29544168381509195,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.18018753902426532,
+          "pitch": 0.12031648787125349,
+          "rotation": 0,
+          "target": "9-sf-47_access-road-1"
+        },
+        {
+          "yaw": 0.17339766354889363,
+          "pitch": 0.3247098225458256,
+          "rotation": 0,
+          "target": "6-sf-47_wellhead--cellar"
+        },
+        {
+          "yaw": -0.42926544227092833,
+          "pitch": 0.4252797216644755,
+          "rotation": 0,
+          "target": "4-sf-47_waste-pit"
+        },
+        {
+          "yaw": 0.7417891469835176,
+          "pitch": 0.5963718657775932,
+          "rotation": 0,
+          "target": "2-sf-47_wellpad-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sf-47_wellpad-1",
+      "name": "SF-47_Wellpad 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 1.967692642928923,
+        "pitch": 0.2564948933939668,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.771419144700051,
+          "pitch": 0.0800786277640757,
+          "rotation": 0,
+          "target": "11-sf-47_wellpad-4"
+        },
+        {
+          "yaw": 1.3423829783151522,
+          "pitch": 0.029459445366951442,
+          "rotation": 0,
+          "target": "2-sf-47_wellpad-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-sf-47_wellpad-2",
+      "name": "SF-47_Wellpad 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 2.347024334887773,
+        "pitch": 0.0868179662991082,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.013058575332413,
+          "pitch": 0.04197160966462121,
+          "rotation": 0,
+          "target": "1-sf-47_wellpad-1"
+        },
+        {
+          "yaw": 2.5946538469751133,
+          "pitch": 0.05984285277020973,
+          "rotation": 0,
+          "target": "5-sf-47_wellhead-sf-47"
+        },
+        {
+          "yaw": 1.4065827628197205,
+          "pitch": 0.03641629543944802,
+          "rotation": 0,
+          "target": "3-sf-47_wellpad-3"
+        },
+        {
+          "yaw": 0.06386762632358156,
+          "pitch": -0.6099942401010559,
+          "rotation": 0,
+          "target": "0-sf-47_overview"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-sf-47_wellpad-3",
+      "name": "SF-47_Wellpad 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 1.961604016103804,
+        "pitch": 0.06602882860109816,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.765964315048172,
+          "pitch": 0.06111425353869748,
+          "rotation": 0,
+          "target": "2-sf-47_wellpad-2"
+        },
+        {
+          "yaw": 1.0223531652658409,
+          "pitch": 0.035305760115386775,
+          "rotation": 0,
+          "target": "4-sf-47_waste-pit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-sf-47_waste-pit",
+      "name": "SF-47_Waste Pit",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.31781226272207697,
+        "pitch": 0.1921649542492485,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5664250624555898,
+          "pitch": 0.017090532466628616,
+          "rotation": 0,
+          "target": "7-sf-47_security-building"
+        },
+        {
+          "yaw": 2.140862856183343,
+          "pitch": 0.032487372479511833,
+          "rotation": 0,
+          "target": "5-sf-47_wellhead-sf-47"
+        },
+        {
+          "yaw": -2.0751748174998372,
+          "pitch": 0.0941949373619515,
+          "rotation": 0,
+          "target": "3-sf-47_wellpad-3"
+        },
+        {
+          "yaw": -2.767833527614309,
+          "pitch": -0.3769970345710867,
+          "rotation": 0,
+          "target": "0-sf-47_overview"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-sf-47_wellhead-sf-47",
+      "name": "SF-47_Wellhead SF-47",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.41644253613095294,
+        "pitch": 0.2778288495169896,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1161437674412475,
+          "pitch": 0.05243845409406234,
+          "rotation": 0,
+          "target": "4-sf-47_waste-pit"
+        },
+        {
+          "yaw": 0.5747222851113563,
+          "pitch": 0.21561786146115125,
+          "rotation": 0,
+          "target": "6-sf-47_wellhead--cellar"
+        },
+        {
+          "yaw": 2.9470227323786435,
+          "pitch": 0.10270428913012353,
+          "rotation": 0,
+          "target": "2-sf-47_wellpad-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-sf-47_wellhead--cellar",
+      "name": "SF-47_WellHead & Cellar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6784257082998373,
+          "pitch": 0.25694492813731173,
+          "rotation": 0,
+          "target": "5-sf-47_wellhead-sf-47"
+        },
+        {
+          "yaw": 1.7708099395372345,
+          "pitch": -0.0000566144261995305,
+          "rotation": 0,
+          "target": "7-sf-47_security-building"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-sf-47_security-building",
+      "name": "SF-47_Security building",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3734809776992307,
+          "pitch": -0.04930032547397545,
+          "rotation": 0,
+          "target": "8-sf-47_access-road-2"
+        },
+        {
+          "yaw": 1.616676036679677,
+          "pitch": 0.025046922149794426,
+          "rotation": 0,
+          "target": "11-sf-47_wellpad-4"
+        },
+        {
+          "yaw": -1.7392558917532845,
+          "pitch": 0.08789563285356472,
+          "rotation": 0,
+          "target": "4-sf-47_waste-pit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-sf-47_access-road-2",
+      "name": "SF-47_Access road 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.9439647653100796,
+        "pitch": 0.46679402989182783,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1129997315657327,
+          "pitch": 0.15495794922379247,
+          "rotation": 0,
+          "target": "7-sf-47_security-building"
+        },
+        {
+          "yaw": 1.8823942343452504,
+          "pitch": 0.11062769980511078,
+          "rotation": 0,
+          "target": "10-sf-47_access-road-to-workover-camp"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-sf-47_access-road-1",
+      "name": "SF-47_Access road 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 2.999917753357135,
+        "pitch": 0.38884427417619705,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.093660284026372,
+          "pitch": -0.27502531514041983,
+          "rotation": 0,
+          "target": "0-sf-47_overview"
+        },
+        {
+          "yaw": 2.717610280913977,
+          "pitch": 0.085322487918436,
+          "rotation": 0,
+          "target": "10-sf-47_access-road-to-workover-camp"
+        },
+        {
+          "yaw": -2.9579873279628117,
+          "pitch": 0.10308973958612988,
+          "rotation": 0,
+          "target": "8-sf-47_access-road-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-sf-47_access-road-to-workover-camp",
+      "name": "SF-47_Access road to workover camp",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.025387941485242038,
+        "pitch": 0.09839771753727788,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2336719886503964,
+          "pitch": 0.049318480129727504,
+          "rotation": 0,
+          "target": "11-sf-47_wellpad-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-sf-47_wellpad-4",
+      "name": "SF-47_Wellpad 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.7696710745956139,
+        "pitch": 0.2164749785819886,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7762976018631136,
+          "pitch": 0.08928386671586352,
+          "rotation": 0,
+          "target": "7-sf-47_security-building"
+        },
+        {
+          "yaw": -2.99168838625995,
+          "pitch": 0.1042650772949898,
+          "rotation": 0,
+          "target": "10-sf-47_access-road-to-workover-camp"
+        },
+        {
+          "yaw": 0.381796249347385,
+          "pitch": 0.07087780830531187,
+          "rotation": 0,
+          "target": "1-sf-47_wellpad-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "WELL PAD SF-47",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
